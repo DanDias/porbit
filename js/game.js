79fd748c1ac9a37db77230b25e972b11f4df641a
Dan Dias <dan.j.dias@gmail.com>
2019-01-01 18:24:58 +0000

    Lines from simulated body to show result

--- js/game.js
+++ js/game.js
@@ -26,7 +26,8 @@ var planetMass = 597200;
 var flickScale = 1;
 var inaccuracy = 1;
 
-var flickEstimate = null;
+var flickEstimate = {};
+var simulationSteps = 100;
 var graphics;
 var destruction = true;
 
@@ -64,17 +65,31 @@ function create ()
         repeat: 0
     };
 
+    flickEstimate = this.physics.add.sprite(0,0,'');
+    flickEstimate.body.mass = pointMass;
+    flickEstimate.body.radius = 16;
+    flickEstimate.body.enable = false;
+    flickEstimate.visible = false;
+
     this.anims.create(animConfig);
 
     //  Events
 
     this.input.on('pointerdown', function (pointer) {
         if (pointer.event.cancelBubble == true) {return;}
-        flickEstimate = new Phaser.Geom.Ellipse(pointer.x,pointer.y,1,1);
+        
+        flickEstimate.body.velocity = Phaser.Math.Vector2.ZERO;
+
+        var points = simulateFrom(flickEstimate,pointer.x,pointer.y,simulationSteps);
+        for(var i=0;i<points.length-1;i++)
+        {
+            drawLine(points[i].x,points[i].y,points[i+1].x,points[i+1].y,2,0xff0ff);
+        }
     });
 
     this.input.on('pointerup', function (pointer) {
         if (!flickEstimate) {return;}
+        graphics.clear();
         var point = this.physics.add.sprite(pointer.downX,pointer.downY,'point');
         point.body.mass = pointMass;
         point.body.radius = 16;
@@ -86,44 +101,206 @@ function create ()
         // Flick to fling
         point.body.velocity.setTo((pointer.upX-pointer.downX)*flickScale,(pointer.upY-pointer.downY)*flickScale);
 
-        //point.orbit = new Phaser.Geom.Ellipse(pointer.x-10,pointer.y-10,50,80);
-        
         point.setScale(pointScale);
         points.push(point);
-        flickEstimate = null;
-
     },this);
 
     this.input.on('pointermove', function (pointer) {
-        if (!flickEstimate) {return;}
+        if(!pointer.isDown) { return; }
+        // Only do stuff if the pointer is down
         graphics.clear();
 
-        drawArrow(pointer.downX,pointer.downY,pointer.x,pointer.y);
-        var dist = Math.sqrt(Math.pow(planet.x-pointer.x,2)+Math.pow(planet.y-planet.y,2));
-        var estimatedVelocity = {
-            x: (pointer.x-pointer.downX)*flickScale,
-            y: (pointer.y-pointer.downY)*flickScale
-        };
-
-        var center = new Phaser.Geom.Point(pointer.x-(9.8+estimatedVelocity.x),pointer.y);
-
-        flickEstimate.x = center.x;
-        flickEstimate.y = center.y;
-        flickEstimate.width = estimatedVelocity.x*2;
-        flickEstimate.height = estimatedVelocity.y*2;
-
-        drawCircle(new Phaser.Geom.Circle(center.x,center.y,3));
-        drawEllipse(flickEstimate);
+        flickEstimate.body.velocity.setTo((pointer.x-pointer.downX)*flickScale,(pointer.y-pointer.downY)*flickScale);
+        var points = simulateFrom(flickEstimate,pointer.downX,pointer.downY,simulationSteps);
+        for(var i=0;i<points.length-1;i++)
+        {
+            drawLine(points[i].x,points[i].y,points[i+1].x,points[i+1].y,2,0xff0ff);
+        }
     });
 }
 
 function update(elapsedTime, delta)
-{
-    // 60 FPS
-    delta /= 60;
+{ 
     points.forEach((p,idx) => {
         updateBody(p,idx);
     });
+}
+
+function simulateFrom(obj,x,y,steps)
+{
+    if (steps === undefined)
+        steps = 1000;
+    var delta = 16.666599999999743/steps;
+    var points = [];
+    obj.x = x;
+    obj.y = y;
+    obj.body.enable = true;
+
+    // TODO: do smarter updates?
+    for(var i=0;i<steps;i++)
+    {
+        var cont = simulateUpdate(obj,delta);
+        obj.x += obj.body.velocity.x * delta;
+        obj.y += obj.body.velocity.y * delta;
+        points[i] = new Phaser.Math.Vector2(obj.x,obj.y);
+        if (!cont)
+            break;
+    }
+
+    obj.body.enable = false;
+    
+    return points;
+}
+
+function accelerateToObject(gameObject,to,speed)
+{
+    var angle = Math.atan2(to.y - gameObject.y, to.x - gameObject.x);
+
+    gameObject.body.acceleration.setToPolar(angle, speed);
+    
+    return angle;
+}
+
+function simulateUpdate(obj, delta) {
+    if (delta===undefined) { delta = 0.1; }
+    obj.body.acceleration.setTo(0, 0);
+    //var scene = ast_.scene;
+    //Loop around all points
+    points.forEach(function(item, ast) {
+        var ax = points[ast].body.acceleration.x;
+        var ay = points[ast].body.acceleration.y;
+        var dx = points[ast].x - item.x;
+        var dy = points[ast].y - item.y;
+        var r = dx * dx + dy * dy;
+        if (r > item.radius * item.radius) {       
+            //The force on a body is (body.mass)/r^2 so taking every non planet body with       
+            // mass=1 the acceleration = force,  (r is already squared save us a sqrt)         
+ 
+            accelerateToObject(points[ast], item, (((item.body.mass) / r)));
+                    
+            ax = ax + points[ast].body.acceleration.x;         
+            ay = ay + points[ast].body.acceleration.y;         
+            ast.body.acceleration.setTo(ax, ay);       
+        }     
+    }, this, false, obj);
+    //Accelerate to planet
+    var ax = obj.body.acceleration.x;
+    var ay = obj.body.acceleration.y;
+    var dx = obj.x - planet.x;
+    var dy = obj.y - planet.y;     
+    var r = dx * dx + dy * dy;
+    if (r < planet.radius * planet.radius) { //|| (r > killRadius * killRadius)) {       
+        return false;
+    } else {
+        accelerateToObject(obj, planet, (planet.body.mass / r));
+        ax = ax + obj.body.acceleration.x;       
+        ay = ay + obj.body.acceleration.y;       
+        obj.body.acceleration.setTo(ax, ay);
+    }
+    computeVelocity(obj.body,delta);
+    return true;
+}
+
+function computeVelocity(body, delta)
+{
+    var velocityX = body.velocity.x;
+    var accelerationX = body.acceleration.x;
+    var dragX = body.drag.x;
+    var maxX = body.maxVelocity.x;
+
+    var velocityY = body.velocity.y;
+    var accelerationY = body.acceleration.y;
+    var dragY = body.drag.y;
+    var maxY = body.maxVelocity.y;
+
+    var speed = body.speed;
+    var allowDrag = body.allowDrag;
+    var useDamping = body.useDamping;
+
+    if (body.allowGravity)
+    {
+        var gravX = config.physics.arcade.gravity.x ? config.physics.arcade.gravity.x : 0;
+        var gravY = config.physics.arcade.gravity.y ? config.physics.arcade.gravity.y : 0;
+        velocityX += (gravX + body.gravity.x) * delta;
+        velocityY += (gravY + body.gravity.y) * delta;
+    }
+
+    if (accelerationX)
+    {
+        velocityX += accelerationX * delta;
+    }
+    else if (allowDrag && dragX)
+    {
+        if (useDamping)
+        {
+            //  Damping based deceleration
+            velocityX *= dragX;
+
+            if (FuzzyEqual(speed, 0, 0.001))
+            {
+                velocityX = 0;
+            }
+        }
+        else
+        {
+            //  Linear deceleration
+            dragX *= delta;
+
+            if (FuzzyGreaterThan(velocityX - dragX, 0, 0.01))
+            {
+                velocityX -= dragX;
+            }
+            else if (FuzzyLessThan(velocityX + dragX, 0, 0.01))
+            {
+                velocityX += dragX;
+            }
+            else
+            {
+                velocityX = 0;
+            }
+        }
+    }
+
+    if (accelerationY)
+    {
+        velocityY += accelerationY * delta;
+    }
+    else if (allowDrag && dragY)
+    {
+        if (useDamping)
+        {
+            //  Damping based deceleration
+            velocityY *= dragY;
+
+            if (FuzzyEqual(speed, 0, 0.001))
+            {
+                velocityY = 0;
+            }
+        }
+        else
+        {
+            //  Linear deceleration
+            dragY *= delta;
+
+            if (FuzzyGreaterThan(velocityY - dragY, 0, 0.01))
+            {
+                velocityY -= dragY;
+            }
+            else if (FuzzyLessThan(velocityY + dragY, 0, 0.01))
+            {
+                velocityY += dragY;
+            }
+            else
+            {
+                velocityY = 0;
+            }
+        }
+    }
+
+    velocityX = Phaser.Math.Clamp(velocityX, -maxX, maxX);
+    velocityY = Phaser.Math.Clamp(velocityY, -maxY, maxY);
+
+    body.velocity.set(velocityX, velocityY);
 }
 
 function updateBody(ast_,idx) {
@@ -160,10 +337,10 @@ function updateBody(ast_,idx) {
         ast_.destroy();
         points.splice(idx,1);
     } else {
-        scene.physics.accelerateToObject(ast_, planet, (planet.body.mass / r));       
+        scene.physics.accelerateToObject(ast_, planet, (planet.body.mass / r));
         ax = ax + ast_.body.acceleration.x;       
         ay = ay + ast_.body.acceleration.y;       
-        ast_.body.acceleration.setTo(ax, ay);     
+        ast_.body.acceleration.setTo(ax, ay);
     }   
 }
 
